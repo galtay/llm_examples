3bd17a3e33e1227a86eb83559822288fbae124db
Gabriel Altay <gabriel.altay@gmail.com>
2024-11-09 01:03:26 -0500

    simple call example

---
+++ call_openai.py
@@ -0,0 +1,17 @@
+import dotenv
+from openai import OpenAI
+
+env = dotenv.dotenv_values(".env")
+client = OpenAI(
+    api_key=env["OPENAI_API_KEY"],
+)
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Say this is a test",
+        }
+    ],
+    model="gpt-4o-mini",
+)
+print(chat_completion)
